c4e8d04546afe2036fcb588397230c37c1bed028
Randel Ramirez <randel1_23@hotmail.com>
2015-04-06 22:16:01 +0800

    Added files

---
+++ CashMoney.Infrastructure/CashMoneyContext.cs
@@ -0,0 +1,17 @@
+﻿using CashMoney.Core;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashMoney.Infrastructure
+{
+    public class CashMoneyContext :DbContext
+    {
+        public DbSet<Income> Gross { get; set; }
+
+        public DbSet<Expense> Incomes { get; set; }
+    }
+}
